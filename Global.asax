73a7582a0b7523a53943759eebc3ab1343a82648
Tom Nam <A00944112@ad.bcit.ca>
2017-02-09 09:11:04 -0800

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aspnet2Azure.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aspnet2Azure.Startup))]
+namespace Aspnet2Azure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
